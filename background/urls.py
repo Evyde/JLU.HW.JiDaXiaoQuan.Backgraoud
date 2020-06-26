4138111b06e4935e971e600e1e105827cb1c0c7c
ForeverOpp <life.app.hanfeng@hotmail.com>
2020-06-26 12:47:29 +0800

    测试

--- background/urls.py
+++ background/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from jlu import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('jlu/', views.test),
 ]
