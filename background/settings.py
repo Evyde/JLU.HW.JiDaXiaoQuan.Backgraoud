5895ab6c5f2c416ab3eb3e2cf6287dfb5052311d
ForeverOpp <life.app.hanfeng@hotmail.com>
2020-06-30 10:39:16 +0800

    去除一些输出

--- background/settings.py
+++ background/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rag5j_pwp&5rg#)gq5&6z%$0ut-jdd^x+!1b51(hqh+w)#uf!d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-cn'
 
 TIME_ZONE = 'Asia/Shanghai'
 
